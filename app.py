3caa883b0aea8ab827a7934173383e94c37be233
DuhwanJang <lyder86@puroom.net>
2023-02-23 12:17:04 +0900

    fix app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 # Imports
 # ----------------------------------------------------------------------------#
 
-import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, flash, redirect, url_for
@@ -13,89 +12,28 @@ import logging
 from logging import Formatter, FileHandler
 from forms import *
 
-
 # ----------------------------------------------------------------------------#
 # App Config.
 # ----------------------------------------------------------------------------#
-
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object("config")
 db = SQLAlchemy()
 migrate = Migrate()
-# TODO: connect to a local postgresql database
 
 # ----------------------------------------------------------------------------#
 # Models.
 # ----------------------------------------------------------------------------#
-
-
-class Venue(db.Model):
-    __tablename__ = "Venue"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.Text)
-    website_link = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String), nullable=False)
-
-    shows = db.relationship("Show", backref="Venue", lazy=True)
-
-    def __repr__(self):
-        return f"<Venue {self.id} {self.name}>"
-
-
-class Artist(db.Model):
-    __tablename__ = "Artist"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String), nullable=False)
-    website_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.Text)
-
-    shows = db.relationship("Show", backref="Artist", lazy=True)
-
-    def __repr__(self):
-        return f"<Artist {self.id} {self.name}>"
-
-
-class Show(db.Model):
-    __tablename__ = "Show"
-
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return f"<Show {self.id} {self.start_time} aritst {self.artist_id} venue {self.venue_id}>"
-
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 with app.app_context():
     db.init_app(app)
     migrate.init_app(app, db)
     db.create_all()
 
+from models import Venue, Artist, Show
+
 # ----------------------------------------------------------------------------#
 # Filters.
 # ----------------------------------------------------------------------------#
-
-
 def format_datetime(value, format="medium"):
     date = dateutil.parser.parse(value)
     if format == "full":
@@ -110,8 +48,6 @@ app.jinja_env.filters["datetime"] = format_datetime
 # ----------------------------------------------------------------------------#
 # Controllers.
 # ----------------------------------------------------------------------------#
-
-
 @app.route("/")
 def index():
     return render_template("pages/home.html")
